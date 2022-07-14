04db6c582e39e4ea34a5c0bbfc588c4d1314f82f
Cédric Sterckx <20957627+CedricSterckx@users.noreply.github.com>
2022-07-14 17:07:52 +0200

    test chat app socket.io (node and angular)

--- Angular real time chat app/server.js
+++ Angular real time chat app/server.js
@@ -14,12 +14,34 @@ const io = require('socket.io')(http, {
 let userList = new Map();
 
 io.on('connection', (socket) => {
+    let username = socket.handshake.query.username;
+    addUser(username, socket.id);
 
+    socket.broadcast.emit('user-list', [...userList.keys()]);
+
+    socket.on('message', (msg) => {
+       socket.broadcast.emit('message-broadcast', {message: msg, username: username});
+    });
+
+    socket.on('disconnect', (reason) => {
+        deleteUser(username, socket.id);
+    });
 });
 
-function addUser(userName, id) {
-    if (!userList.has(userName)) {
-        userList.set(userName, new Set(id));
+function addUser(username, id) {
+    if (!userList.has(username)) {
+        userList.set(username, new Set(id));
+    } else {
+        userList.get(username).add(id);
+    }
+}
+
+function deleteUser(username) {
+    if (userList.has(username)) {
+        let userIds = userList.get(username);
+        if (userIds.size === 0) {
+            userList.delete(username);
+        }
     }
 }
 
